e620838575f4993ac8e111a8ec342cc0e94dd297
sanujasethi06 <sethisanuja1998@gmail.com>
2024-03-20 15:42:31 +0530

    default commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Lets make intranet solution</h2>
+        
+    </>
+  )
+}
+
+export default App
